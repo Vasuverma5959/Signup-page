ae87d5332aa54aa65a5a005780a36fea31067e8c
Vasu Verma <155059082+Vasuverma5959@users.noreply.github.com>
2024-06-03 08:38:05 +0530

    Add files via upload

---
+++ signup.js
@@ -0,0 +1,16 @@
+function change123(){
+    let b = document.getElementById("main-2")
+    b.style.transform="translateX(-175px)"
+    b.style.transition=".7s ease-in-out"
+    let a = document.getElementById("main")
+    a.style.transform="translateX(175px)"
+    a.style.transition=".7s ease-in-out"
+    // alert('hello')
+}
+function change321(){
+    let d = document.getElementById("main-2")
+    d.style.transform="translateX(-1125px)"
+    let c = document.getElementById("main")
+    c.style.transform="translateX(175px)"
+    
+}
